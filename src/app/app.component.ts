575d68e297b76f50807393facf7255d1c8e1e6e6
Kamoullox <62745308+Kamoullox@users.noreply.github.com>
2021-12-27 23:11:05 +0100

    maj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
@@ -8,36 +7,8 @@ import { FaceSnap } from './models/face-snap.model';
 })
 export class AppComponent implements OnInit {
 
-  faceSnaps!: FaceSnap[];
 
   ngOnInit(): void {
-    this.faceSnaps = [
-      {
-        title: 'Archibald',
-        description: "Mon meilleur ami depuis tout petit !",
-        imageUrl: "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg",
-        createdDate: new Date(),
-        snaps: 0,
-        location: 'Paris'
-      },
-
-      {
-        title: "Trio Légendaire !",
-        description: "Personnages principals de Harry potter",
-        imageUrl: "https://img.filmsactu.net/datas/films/h/a/harry-potter-et-les-reliques-de-la-mort-partie-2/xl/harry-potter-et-les-reliques-de-la-mort-partie-2-61c2ef691296c.jpg",
-        createdDate: new Date(),
-        snaps: 125,
-        location: 'Poudlard'
-      },
-
-      {
-        title: "Sylvain Duriff",
-        description: "Le monarque cosmique !",
-        imageUrl: "https://blobsvc.wort.lu/picture/f7ea4fe76f388eb367037d1f6e21e671/800/800/crop/0/109/440/357/wortv3/edd7ee043660c9283a832bb1c4257e6c4e8fffa0",
-        createdDate: new Date(),
-        snaps: 0
-      }
-
-    ]
+   
   }
 }
---
+++ src/app/services/face-snaps.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from "@angular/core";
+import { FaceSnap } from "../models/face-snap.model";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class FaceScnapsService{
+    faceSnaps: FaceSnap[] = [
+        {
+          title: 'Archibald',
+          description: "Mon meilleur ami depuis tout petit !",
+          imageUrl: "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg",
+          createdDate: new Date(),
+          snaps: 0,
+          location: 'Paris'
+        },
+  
+        {
+          title: "Trio Légendaire !",
+          description: "Personnages principaux de Harry potter",
+          imageUrl: "https://img.filmsactu.net/datas/films/h/a/harry-potter-et-les-reliques-de-la-mort-partie-2/xl/harry-potter-et-les-reliques-de-la-mort-partie-2-61c2ef691296c.jpg",
+          createdDate: new Date(),
+          snaps: 125,
+          location: 'Poudlard'
+        },
+  
+        {
+          title: "Sylvain Duriff",
+          description: "Le monarque cosmique !",
+          imageUrl: "https://blobsvc.wort.lu/picture/f7ea4fe76f388eb367037d1f6e21e671/800/800/crop/0/109/440/357/wortv3/edd7ee043660c9283a832bb1c4257e6c4e8fffa0",
+          createdDate: new Date(),
+          snaps: 0
+        }
+      ]
+}
